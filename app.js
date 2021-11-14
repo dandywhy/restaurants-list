8c20dd28a2cb6263fd3053c2bcb268ec60a66e23
Dandy <lavenderflyyaya@gmail.com>
2021-11-15 01:13:56 +0800

    feat: heroku push

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 require('./config/mongoose')
 // require('./routes/modules/sort')
@@ -19,6 +20,4 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
-})
+app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`))
